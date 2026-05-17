5c18512410967e3c53f2813583dfb84254c5643b
Rikki <battlemonkey_926@hotmail.com>
2026-05-17 22:47:44 +1200

    added bug login logging

--- functions/api/auth/login.js
+++ functions/api/auth/login.js
@@ -1,3 +1,6 @@
+console.log("ENV DB:", env.DB);
+console.log("ENV SECRET:", env.OCCULTUS_SECRET);
+
 import { encrypt } from "../../utils/crypto";
 import { generateToken, sessionExpiry } from "../../utils/session";
 
